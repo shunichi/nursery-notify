92d2417002b484fae3162637a52b8895cbb20ca8
Shunichi Ikegami <sike.tm@gmail.com>
2021-01-28 22:36:47 +0900

    tmp

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
 import * as functions from "firebase-functions";
+import * as firebase from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+firebase.initializeApp();
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  firebase.firestore().collection('line').add({token: `example-token-${Date.now().toString()}`})
+  response.send("Hello from Firebase!");
+});
